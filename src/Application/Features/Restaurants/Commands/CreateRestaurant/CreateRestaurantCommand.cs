8cd538d3f082a5f97703da17bfa964cc8f99e200
mahmoud <mahmoudsayed1332002@gmail.com>
2026-02-17 14:48:50 +0200

    create restaurants feature flow

---
+++ src/Application/Features/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommand.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Features.Restaurants.Commands.CreateRestaurant;
+public class CreateRestaurantCommand
+{
+}
