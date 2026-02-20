abc3fa964e5a63f1bc52886f6060a2a1b5148074
mahmoud <mahmoudsayed1332002@gmail.com>
2026-02-20 17:52:35 +0200

    add user preferences table and configurations

--- src/Domain/Entities/Users/ApplicationUser.cs
+++ src/Domain/Entities/Users/ApplicationUser.cs
@@ -8,6 +8,7 @@ public abstract class ApplicationUser : IdentityUser<Guid>
     public abstract string MainRole { get; set; }
     public bool IsActive { get; set; } = true;
     public bool IsDeleted { get; set; }
+    public ICollection<UserPreference>? Preferences { get; set; }
 }
 
 public class Admin : ApplicationUser
---
+++ src/Infrastructure/Data/Migrations/20260220154846_Add-UserPreferences-Table.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserPreferencesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserPreference",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Key = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPreference", x => new { x.UserId, x.Key });
+                    table.ForeignKey(
+                        name: "FK_UserPreference_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserPreference");
+        }
+    }
+}
