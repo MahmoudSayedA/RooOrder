6348bf71629b8a7f8d814e9f4420577efc3671c5
mahmoud <mahmoudsayed1332002@gmail.com>
2026-02-20 16:25:08 +0200

    add properties to current user service

--- src/Application/Identity/Services/IUser.cs
+++ src/Application/Identity/Services/IUser.cs
@@ -1,6 +1,15 @@
-﻿namespace Application.Identity.Services;
+﻿using System.Security.Claims;
+
+namespace Application.Identity.Services;
 
 public interface IUser
 {
     string? Id { get; }
+    public Guid? IdGuid => Guid.TryParse(Id, out var guid) ? guid : null;
+    public string? UserName { get; }
+    public string? Email { get; }
+    public IEnumerable<string> Roles { get; }
+
+    public bool IsAuthenticated { get; }
+
 }
