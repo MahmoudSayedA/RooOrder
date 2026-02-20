26554ef288685c9e4b7dadb7145ffb876590c7e0
mahmoud <mahmoudsayed1332002@gmail.com>
2026-02-20 22:10:20 +0200

    add get all users, update user preferences endpoints.

--- src/Web/Controllers/UsersController.cs
+++ src/Web/Controllers/UsersController.cs
@@ -1,6 +1,9 @@
-﻿using Application.Features.Users.Models;
+﻿using Application.Features.Users.Commands.UpdateUserPreferences;
+using Application.Features.Users.Models;
 using Application.Features.Users.Queries.GetAllUsersForAdmin;
+using Application.Features.Users.Queries.GetMyProfile;
 using Application.Identity.Services;
+using Domain.Constants;
 using Microsoft.AspNetCore.Authorization;
 
 namespace Web.Controllers;
@@ -20,19 +23,29 @@ public class UsersController: ControllerBase
     }
 
     [HttpPost("get-all")]
-    [Authorize(Roles = "Admin")]
+    [Authorize(Roles = Roles.Admin)]
     public async Task<IActionResult> GetAllUsers(GetAllUsersForAdminQuery query, CancellationToken cancellationToken)
     {
         var result = await _mediator.Send(query, cancellationToken);
         return Ok(result);
     }
 
-    //[HttpGet("my-info")]
-    //[Authorize]
-    //public async Task<IActionResult> GetCurrentUser(CancellationToken cancellationToken)
-    //{
-        
-    //}
+    [HttpGet("my-profile")]
+    [Authorize]
+    public async Task<IActionResult> GetMyProfile(CancellationToken cancellationToken)
+    {
+        var query = new GetMyProfileQuery();
+        var profile = await _mediator.Send(query, cancellationToken);
+        return Ok(profile);
+    }
 
+
+    [HttpPut("update-preferences")]
+    [Authorize]
+    public async Task<IActionResult> UpdatePreferences([FromBody] UpdateUserPreferencesCommand command, CancellationToken cancellationToken)
+    {
+        await _mediator.Send(command, cancellationToken);
+        return NoContent();
+    }
 
 }
