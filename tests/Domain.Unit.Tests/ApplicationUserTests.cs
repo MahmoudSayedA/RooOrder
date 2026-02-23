0a11db666f08a3d4c3f53315f2631fdaf4abf8d8
mahmoud <mahmoudsayed1332002@gmail.com>
2026-02-24 01:21:31 +0200

    add domain tests for ApplicationUser

---
+++ tests/Domain.Unit.Tests/ApplicationUserTests.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities.Users;
+
+namespace Domain.Unit.Tests;
+public class ApplicationUserTests
+{
+
+    [Fact]
+    public void ApplicationUser_MainRole_ReturnsCorrectRole()
+    {
+        // Arrange
+        var admin = new Admin();
+        var customer = new Customer();
+        var restaurantOwner = new RestaurantOwner();
+        // Act & Assert
+        Assert.Equal(Constants.Roles.Admin, admin.MainRole);
+        Assert.Equal(Constants.Roles.Customer, customer.MainRole);
+        Assert.Equal(Constants.Roles.RestaurantOwner, restaurantOwner.MainRole);
+    }
+}
